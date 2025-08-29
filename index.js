c9d9f80334095de5d184005d0d24282fdb8bdd50
Soufianelahmid <soufiane.lahmid7@gmail.com>
2025-08-29 22:25:30 +0100

    adding 2 file

---
+++ index.js
@@ -0,0 +1,107 @@
+//console.log('learn javascript');
+//console.log('how can i built it');
+
+//window.alert('hey there how are youu doing now');
+//window.alert('this is my first step to be a full-stack developer');
+
+//document.getElementById("Myfirstname").textContent = "Sufiane";
+//document.getElementById("Mylastname").textContent = "Lahmid";
+
+
+  
+// to do a comment by this or this under
+/* this 
+is 
+a 
+comment 
+*/
+
+
+// varibal = a container that stores a value, behaves as if it were 
+//              content.
+// 1. declaration let x;
+// 2. assignement x = 100;
+
+// NUMBERS
+
+//let age = 27;
+//let price = 11.99;
+//let gpa = 3.1;
+
+//console.log(typeof age);
+//console.log(`You are ${age} years old`);
+//console.log(`The price is ${price}`);
+//console.log(`Your gpa is: ${gpa}`);
+
+ //==) STRINGS
+//let firstname = "soufiane";
+//let favoritefood = "djaj";
+//let email = "soufiane.lahmid7@gmail.com";
+
+
+//console.log(typeof email);
+//console.log(typeof firstname);
+//console.log(`your name is ${firstname}`);
+//console.log(`i like ${favoritefood}`);
+
+//==) Booleans 
+
+//let online = true;
+//let offline = false;
+//let forsale = false;
+
+
+//console.log(`is this car for sale: ${forsale}`);
+//console.log(`soufiane is ${offline}`);
+//console.log(`you are ${online}`);
+
+
+//let fullname = "soufiane";
+//let age = 26;
+//let student = true;
+
+//document.getElementById("p1").textContent = `your name is: ${fullname}`;
+//document.getElementById("p3").textContent = `you are  ${student}`;
+//document.getElementById("p2").textContent = `your are is: ${age}`;
+
+
+//===================================================================
+
+
+// ARITHMETIC OPERATORS = operands ( values , variables , et ) 
+//                        operators (* / + -) ex. 11 = x + 5; 
+//                        X = 11 - 5 
+//                        X = 6 
+
+//let students = 35; 
+
+//students = students + 5;  
+//students = students - 5;
+//students = students * 5;
+//students = students / 5;
+
+//students += 5; 
+//students -= 5;
+//students *= 5; 
+//students /= 5;
+
+
+//students **= 5;
+//students++;
+//students--;
+
+//students %= 3;
+//console.log(students)
+
+/* operator precendence
+               1. parenthesis 
+               2. exponents
+               3. multiplication & division & modulo
+               4. addition & subtraction
+ */
+ // example : 
+
+  //]let result = 1 + 2 + 3 * 5 + 2 ** 4;
+  //console.log(result);
+
+  
